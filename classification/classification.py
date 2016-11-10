65815fc5cf4166d6a98e419e835b6ad3192eab0b
cbarGit <antisenso@gmail.com>
2016-11-10 20:05:07 +0100

    classification update

--- classification/classification.py
+++ classification/classification.py
@@ -2,57 +2,45 @@
 #
 # data samples from https://archive.ics.uci.edu/ml/machine-learning-databases/wine/
 #
+#module csv for reading from csv file
 #module "random" for random number
+#mosule numpy for Numpy library
 #module svm from sklearn for classification algorithm
+import csv
 import random
+import numpy as np
 from sklearn import svm
 
-#Cleaning part:
-#to refine the data samples
-buff = []
 data = []
 
-#read from file and append data to a list, removing "\n" char at the end of
-#every line
+#read from file and create an Numpy array
 with open("wine.data") as dataFile:
-    for row in dataFile:
-        buff.append(row.rstrip('\n'))
+    data = np.array([list(map(str,row)) for row in csv.reader(dataFile, delimiter=',')])
 
-#cleaning further, creating an array of arrays
-for row in buff:
-    data.append(row.split(','))
 
 #create 3 random number for selecting test samples
-rNum0 = random.randrange(0,177)
-rNum1 = random.randrange(0,177)
-rNum2 = random.randrange(0,177)
+#test, for the random samples to choose
+r = np.random.randint(0,data.shape[0],3)
+test = data[r]
+print(test)
+print()
+
+#clean the test samples removing first feature (rapresents class)
+test = test[:,1:]
 
 #instList, for the classes or labels of the sklearn's targets
-#test, for the random samples to choose
-instList = []
-test = []
+#data = np.array(data)
+instList = data[:,:1].reshape(-1,).tolist()
 
-#choose the samples
-#remove the first element of every sample, that describe the latter class
-for n,sample in enumerate(data):
-    if(n == rNum0 or n == rNum1 or n == rNum2):
-        print(sample)
-        test.append(sample)
-    instList.append(sample[0])
-    del sample[0]
+#clean data as test samples
+data = data[:,1:]
 
 #assign data and targets
 X, y = data, instList
 
-#show chosen samples
-print()
-for i in test:
-    print(i)
-print()
-
 #load algorithm and fit data and targets into it
 clf = svm.SVC()
-clf.fit(X, y)
+clf.fit(X,y)
 
 #predict on every test samples
 for n,i in enumerate(test):
