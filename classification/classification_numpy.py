fbe1a308b68ecec4f19e9e3a7c2147dec1c13697
cbarGit <antisenso@gmail.com>
2016-11-16 19:23:59 +0100

    using pandas in classification

--- classification/classification_numpy.py
+++ classification/classification_numpy.py
@@ -2,24 +2,30 @@
 #
 # data samples from https://archive.ics.uci.edu/ml/machine-learning-databases/wine/
 #
-#module numpy for Numpy library
-#module pandas for Pandas library
+#module csv for reading from csv file
+#module "random" for random number
+#mosule numpy for Numpy library
 #module svm from sklearn for classification algorithm
-import pandas as pd
+import csv
+import random
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,confusion_matrix
 
-data = pd.read_csv('wine.data', delimiter=',', header=None)
 
-y, X = data[[0]], data.drop(0, axis=1)
+#read from file and create an Numpy array
+with open("wine.data") as dataFile:
+    data = np.array([list(map(str,row)) for row in csv.reader(dataFile, delimiter=',')])
 
+#assign data and targets
+X, y = data[:,1:], data[:,:1]
+#print(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
 #load algorithm and fit data and targets into it
 clf = svm.SVC()
-clf.fit(X,y.values.ravel())
+clf.fit(X,y.ravel())
 
 predictions = clf.predict(X_test)
 
